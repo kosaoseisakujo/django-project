99f20b2c9f1a950cc391cf9cfb04fdcdad4bf677
kosaoseisakujo <kosaoseisakujo@gmail.com>
2021-08-20 14:56:23 +0900

    viewに特集記事取得

--- top/views.py
+++ top/views.py
@@ -10,10 +10,11 @@ def post_list(request):
     persons = Person.objects.all()
     # アパート情報
     apartments = Apartment.objects.all()
-
+    featured_apartments = Apartment.objects.filter(features_flag=1)
     context ={
         'main_menus': main_menus,
         'persons': persons,
         'apartments': apartments,
+        'featured_apartments': featured_apartments,
     }
     return render(request, 'top/post_list.html', context)
