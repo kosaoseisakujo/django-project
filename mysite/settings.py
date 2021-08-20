b46b66c7ca64dfd035043ef9eec7a5bd8d78b403
kosaoseisakujo <kosaoseisakujo@gmail.com>
2021-08-20 10:58:25 +0900

    news app作成

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'top.apps.TopConfig',
+    'news.apps.NewsConfig',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('top.urls')),
+    path('news/', include('news.urls')),
 ]
