5a2ed927176ae4ccbe34abfad59b0cd551f0060d
kosaoseisakujo <kosaoseisakujo@gmail.com>
2021-08-20 11:19:08 +0900

    news一覧ページ リンク以外を変数出力に変更

--- news/views.py
+++ news/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Notice
 
 # Create your views here.
 def news(request):
+    notices = Notice.objects.all()
     context ={
-
+        'notices': notices
     }
     return render(request, 'top/news.html', context)
