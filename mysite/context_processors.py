4ffbf6ff97134cf33faa95f73f8d2b35f0a65705
kosaoseisakujo <kosaoseisakujo@gmail.com>
2021-08-24 17:36:39 +0900

    全ページ共通メニュー作成

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -7,9 +7,11 @@ def some_processor(request: HttpRequest):
     # dic = create_dict()
     # return dic
     GLOBAL_MENU = Menu.objects.filter(main_flag=1)
+    FOOTER_MENU = Menu.objects.all()
     return {
         'IMAGE_PATH': 'https://cosaoyuji.s3.ap-northeast-1.amazonaws.com/images/',
         'IMAGE_PATH_TEST': 'https://cosaoyuji.s3.ap-northeast-1.amazonaws.com/',
         'TEST': 'TEST',
-        'GLOBAL_MENU': GLOBAL_MENU
+        'GLOBAL_MENU': GLOBAL_MENU,
+        'FOOTER_MENU': FOOTER_MENU,
     }
